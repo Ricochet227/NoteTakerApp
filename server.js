17b873a1469453219cf33eba980c929643b778ac
Ricochet227 <skyler7crawford@gmail.com>
2023-11-20 14:03:11 -0700

    added npm uniqid

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'app', 'public')));
 
-require('./app/routing/api-routes')(app);
-require('./app/routing/html-routes')(app);
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
