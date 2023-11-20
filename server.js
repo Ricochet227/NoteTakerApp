6e91dce4f12623287920cae7a787af7ac90e263d
Ricochet227 <skyler7crawford@gmail.com>
2023-11-20 15:08:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 27801;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
